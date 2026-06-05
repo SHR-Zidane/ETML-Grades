ce90ed961843471d92c858d9c355e13691267d88
Zidane Sahraoui <pd01wvj@eduvaud.ch>
2026-06-05 16:21:25 +0200

    chore(test): mise en place d'un test

--- main.c
+++ main.c
@@ -2,7 +2,22 @@
 #include "report.h"
 
 int main(){
-    printf("test");
+    Grade C295[] = {
+        {3.5f, true, 4.0f},
+        {1.0f, true, 6.0f}
+    };
+    Grade I426[] = {
+        {4.0f, false, 0.0f},
+        {4.5f, false, 0.0f}
+    };
 
+    Subject MoyenneInf[] = {
+        {"I426", I426, 2, 'I'},
+        {"C295", C295, 2, 'C'}
+    };
+
+    int NbSubject = sizeof(MoyenneInf) / sizeof(MoyenneInf[0]);
+    float AvgInf = AvgInfo(MoyenneInf, NbSubject);
+    printf("moyenne en info : %f", AvgInf);
     return 0;
 }
