1e085f64437385b6f3a872412221cfe7acd6f79a
Zidane Sahraoui <pd01wvj@eduvaud.ch>
2026-06-10 11:25:40 +0200

    feat(gui): implement base callback structure for adding grades

--- gui.c
+++ gui.c
@@ -4,9 +4,19 @@
 #include "glibconfig.h"
 #include "report.h"
 
+typedef struct {
+    GtkWidget *combo_nature;
+    GtkWidget *entryName;
+    GtkWidget *entryGrade;
+    GtkWidget *checkWeight;
+    GtkWidget *entryWeight;
+    GtkWidget *vboxList;
+} GradeForm;
+
 static void on_destroy(GtkWidget *widget, gpointer data) {
     gtk_main_quit();
 }
+
 void on_weightToggled(GtkWidget *checkWidget, gpointer data) {
     //récupération de l'état du checkbutton (si il est coché ou pas)
     gboolean isChecked = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(checkWidget));
@@ -20,6 +30,11 @@ void on_weightToggled(GtkWidget *checkWidget, gpointer data) {
         gtk_widget_set_visible(entry, FALSE);
     }
 }
+
+void on_addGrade(GtkWidget *button, gpointer data){
+    GradeForm *form = data;
+}
+
 void create_main_window(int argc, char *argv[]) {
     GtkWidget *window;
     GtkWidget *vbox_main;
@@ -27,17 +42,14 @@ void create_main_window(int argc, char *argv[]) {
     GtkWidget *hbox_header;
     GdkPixbuf *icon;
     GtkWidget *combo_year;
-    GtkWidget *combo_nature;
     GtkWidget *hbox_columns;
     GtkWidget *vbox_sem1;
     GtkWidget *vbox_sem2;
     GtkWidget *vbox_avg;
     GtkWidget *hbox_add_grade;
-    GtkWidget *entryName;
-    GtkWidget *entryGrade;
     GtkWidget *btn_add;
-    GtkWidget *checkWeight;
-    GtkWidget *entryWeight;
+    GtkWidget *scroll_sem1;
+    static GradeForm Sem1Form;
 
     gtk_init(&argc, &argv);
 
@@ -81,37 +93,45 @@ void create_main_window(int argc, char *argv[]) {
     vbox_sem2 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     vbox_avg = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
 
-    entryName = gtk_entry_new();
-    entryGrade = gtk_entry_new();
-    checkWeight = gtk_check_button_new_with_label("Pondéré ?:");
-    entryWeight = gtk_entry_new();
+    Sem1Form.entryName = gtk_entry_new();
+    Sem1Form.entryGrade = gtk_entry_new();
+    Sem1Form.checkWeight = gtk_check_button_new_with_label("Pondéré ?:");
+    Sem1Form.entryWeight = gtk_entry_new();
     btn_add = gtk_button_new_with_label("+");
 
-    gtk_entry_set_placeholder_text(GTK_ENTRY(entryName), "Name");
-    gtk_entry_set_placeholder_text(GTK_ENTRY(entryGrade), "Note");
-    gtk_entry_set_placeholder_text(GTK_ENTRY(entryWeight), "%");
+    gtk_entry_set_placeholder_text(GTK_ENTRY(Sem1Form.entryName), "Name");
+    gtk_entry_set_placeholder_text(GTK_ENTRY(Sem1Form.entryGrade), "Note");
+    gtk_entry_set_placeholder_text(GTK_ENTRY(Sem1Form.entryWeight), "%");
 
-    gtk_entry_set_width_chars(GTK_ENTRY(entryWeight), 4);
+    gtk_entry_set_width_chars(GTK_ENTRY(Sem1Form.entryWeight), 4);
 
     hbox_add_grade = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 15);
 
-    combo_nature = gtk_combo_box_text_new();
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_nature), "C");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_nature), "I");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_nature), "Maths");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_nature), "Anglais");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_nature), "ECG");
-    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_nature), 0);
+    Sem1Form.combo_nature = gtk_combo_box_text_new();
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(Sem1Form.combo_nature), "C");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(Sem1Form.combo_nature), "I");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(Sem1Form.combo_nature), "Maths");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(Sem1Form.combo_nature), "Anglais");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(Sem1Form.combo_nature), "ECG");
+    gtk_combo_box_set_active(GTK_COMBO_BOX(Sem1Form.combo_nature), 0);
 
-    gtk_box_pack_start(GTK_BOX(hbox_add_grade), entryName, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox_add_grade), combo_nature, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox_add_grade), entryGrade, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox_add_grade), checkWeight, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox_add_grade), entryWeight, FALSE, FALSE, 0);
-    gtk_widget_set_visible(entryWeight, FALSE);
-    gtk_widget_set_no_show_all(entryWeight, TRUE);
-    g_signal_connect(checkWeight, "toggled", G_CALLBACK(on_weightToggled), entryWeight);
+    gtk_box_pack_start(GTK_BOX(hbox_add_grade), Sem1Form.entryName, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox_add_grade), Sem1Form.combo_nature, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox_add_grade), Sem1Form.entryGrade, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox_add_grade), Sem1Form.checkWeight, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox_add_grade), Sem1Form.entryWeight, FALSE, FALSE, 0);
+    gtk_widget_set_visible(Sem1Form.entryWeight, FALSE);
+    gtk_widget_set_no_show_all(Sem1Form.entryWeight, TRUE);
+    g_signal_connect(Sem1Form.checkWeight, "toggled", G_CALLBACK(on_weightToggled), Sem1Form.entryWeight);
     gtk_box_pack_start(GTK_BOX(hbox_add_grade), btn_add, FALSE, FALSE, 0);
+    scroll_sem1 = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll_sem1), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+
+    Sem1Form.vboxList = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+
+    gtk_container_add(GTK_CONTAINER(scroll_sem1), Sem1Form.vboxList);
+
+    gtk_box_pack_start(GTK_BOX(vbox_sem1), scroll_sem1, TRUE, TRUE, 0);
 
     // Section Semestre 1
     gtk_box_pack_start(GTK_BOX(vbox_sem1), gtk_label_new("Semester 1"), FALSE, FALSE, 0);
